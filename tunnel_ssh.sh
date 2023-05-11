52beaf5db56a9a1073eea3e80400888cf5d88cd1
joker <geovannicinto@gmail.com>
2023-05-11 13:42:45 -0600

    Se añadio función para crear archivos en systemd e iniciarlo

--- tunnel_ssh.sh
+++ tunnel_ssh.sh
@@ -23,14 +23,38 @@ connect_ssh() {
   fi
 }
 
+create_file() {
+  file=/etc/systemd/user
+  file_ssh=$file/tunnel.sh
+  file_service=$file/tunnel.service
+  mkdir -p $file
+  wget -O $file_ssh https://raw.githubusercontent.com/axolotl13/Scripts-Linux/main/tunnel_ssh.sh
+  touch $file_service
+
+  echo "[Service]
+ExecStart=/bin/sh $file_ssh
+
+[Install]
+WantedBy=default.target" >$file_service
+
+  sleep 3
+  systemctl --user daemon-reload
+  if [ "$(systemctl --user is-enabled tunnel)" == "disabled" ]; then
+    sudo systemctl enable tunnel
+    # sudo systemctl start tunnel
+  fi
+}
+
 check_ssh() {
   if [ "$(systemctl is-active sshd)" == "active" ]; then
     echo "SSH service is running."
+    create_file
     # connect_ssh
   else
     echo "SSH service is not running."
     sudo systemctl enable sshd
     sudo systemctl start sshd
+    create_file
     # connect_ssh
   fi
 }
@@ -44,16 +68,16 @@ check_os() {
     exit 1
   fi
 
-  if [ -z "$(which sudo)" ] && [ -z "$(which sshd)" ]; then
+  if [ -z "$(which sudo)" ] || [ -z "$(which sshd)" ] || [ -z "$(which wget)" ]; then
     case $OS in
     "Ubuntu" | "Debian GNU/Linux")
-      apt update && apt upgrade && apt install sudo openssh-server -y
+      apt update && apt upgrade && apt install sudo openssh-server wget -y
       ;;
     "Fedora")
-      dnf upgrade && dnf install sudo openssh-server -y
+      dnf upgrade && dnf install sudo openssh-server wget -y
       ;;
     "Arch Linux")
-      pacman -Syu && pacman -Sy sudo openssh
+      pacman -Syu && pacman -Sy sudo openssh wget
       ;;
     *)
       echo "No se reconoce la distribución $OS"
